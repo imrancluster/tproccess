370a587a9b132bb0dfde19470b80cddf26b0c8fa
Imran Sarder <imrancluster@gmail.com>
2025-02-15 18:56:12 +0600

    added exe build note

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # tproccess
+
+## Install the fyne
+
+```bash
+go install fyne.io/fyne/v2/cmd/fyne@latest
+```
+
+## Windows App build using Macbook M2 Pro
+
+- Make sure there is a icon called `Icon.png` inside the cmd folder or root folder where the main function located.
+
+**Install the dependencies:**
+
+```bash
+brew install mingw-w64
+```
+
+** Build EXE file using fyne**
+
+```bash
+CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 fyne package -os windows -exe myapp.exe -src ./cmd
+```
+
+** Build EXE file using go build**
+
+```bash
+CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o myapp.exe ./cmd
+```
--- internal/ui/window.go
+++ internal/ui/window.go
@@ -3,7 +3,6 @@ package ui
 import (
 	"image/color"
 	"log"
-	"os"
 
 	"github.com/imrancluster/techmongo/fyne/tprocess/internal/monitoring"
 	"github.com/imrancluster/techmongo/fyne/tprocess/internal/utils"
@@ -16,90 +15,27 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-// NewWindow creates a new Fyne window with the UI components
-func NewWindowOld() fyne.Window {
-	app := fyne.CurrentApp()
-	myWindow := app.NewWindow("Candidate Monitoring App")
-	myWindow.Resize(fyne.NewSize(400, 250)) // Set the standard window size
-
-	// Background Image
-	// Load the SVG image from file
-	svgFile := "tt_v2_logo.svg"
-	if _, err := os.Stat(svgFile); os.IsNotExist(err) {
-		myWindow.SetContent(canvas.NewText("SVG file not found!", theme.ForegroundColor()))
-		myWindow.ShowAndRun()
-	}
-
-	// Create an image canvas from the SVG file
-	bgImage := canvas.NewImageFromFile(svgFile)
-	bgImage.FillMode = canvas.ImageFillStretch
-
-	// Set a light theme
-	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
-
-	label2 := makeLabel("Not Connected", color.RGBA{R: 0, G: 0, B: 0, A: 255}, false)
-	label1 := makeLabel("Connected", color.RGBA{R: 178, G: 222, B: 39, A: 255}, true)
-
-	// Input Field to get Candiate Slug
-	input := widget.NewEntry()
-	input.SetPlaceHolder("Enter Candidate ID")
-
-	// Connect button
-	btn := widget.NewButton("Connect", func() {
-		// Input text processing
-		utils.Logger().Println("Input Text: " + input.Text)
-		if input.Text == "" {
-			dialog.ShowInformation("Error", "Please enter your candidate ID. You should get the ID on the browser!", myWindow)
-			return
-		}
-
-		// status.SetText("Connected")
-		utils.Logger().Println("Button clicked. Monitoring started...")
-
-		label2.Hidden = true
-		label1.Hidden = false
-
-		go func() {
-			err := monitoring.StartMonitoring()
-			if err != nil {
-				log.Printf("Monitoring error: %v", err)
-			}
-		}()
-
-		// Start keyboard monitoring in a separate goroutine
-		go monitoring.StartKeyboardMonitoring()
-	})
-
-	// Arrange components in a vertical layout
-	content := container.NewVBox(
-		bgImage,
-		input,
-		btn,
-		// status,
-		label1,
-		label2,
-	)
-
-	myWindow.SetContent(content)
-	return myWindow
-}
-
 func NewWindow() fyne.Window {
 	app := fyne.CurrentApp()
 	myWindow := app.NewWindow("Candidate Monitoring App")
 	myWindow.Resize(fyne.NewSize(400, 250)) // Set the standard window size
 
-	// Load the SVG image from file
-	svgFile := "tt_v2_logo.png"
-	if _, err := os.Stat(svgFile); os.IsNotExist(err) {
-		myWindow.SetContent(canvas.NewText("SVG file not found!", theme.ForegroundColor()))
-		myWindow.ShowAndRun()
-		return myWindow // Return to avoid further execution
-	}
-
-	// Create an image canvas from the SVG file
-	bgImage := canvas.NewImageFromFile(svgFile)
+	// Load the bundled image
+	imageResource := fyne.NewStaticResource("tt_v2_logo.png", resourceTtv2logoPng.Content())
+	bgImage := canvas.NewImageFromResource(imageResource)
 	bgImage.FillMode = canvas.ImageFillOriginal // Use original size for bottom-centered positioning
+
+	// Load the SVG image from file
+	// svgFile := "tt_v2_logo.png"
+	// if _, err := os.Stat(svgFile); os.IsNotExist(err) {
+	// 	myWindow.SetContent(canvas.NewText("png file not found!", theme.ForegroundColor()))
+	// 	myWindow.ShowAndRun()
+	// 	return myWindow // Return to avoid further execution
+	// }
+
+	// // Create an image canvas from the SVG file
+	// bgImage := canvas.NewImageFromFile(svgFile)
+	// bgImage.FillMode = canvas.ImageFillOriginal // Use original size for bottom-centered positioning
 
 	// Set a light theme
 	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
@@ -111,9 +47,9 @@ func NewWindow() fyne.Window {
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter Candidate ID")
 
-	// Imran Sarder 
-	// skdjfksdlj Imran kimran kjskdf 
-	// Imran 
+	// Imran Sarder
+	// skdjfksdlj Imran kimran kjskdf
+	// Imran
 
 	// Connect button
 	btn := widget.NewButton("Connect", btnFunc(input, label2, label1, myWindow))
